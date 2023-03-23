86eca7b30abd92fc6fd5b175c97ad5483d7c4069
Vladimir Siman <online@clew.cz>
2023-03-23 15:23:40 +0100

    fix dependencies

--- gatekeeper-policies.tf
+++ gatekeeper-policies.tf
@@ -9,6 +9,8 @@ resource "helm_release" "gatekeeper-templates" {
   version   = "1.0.0"
   namespace = "gatekeeper-system"
   values    = [data.template_file.gatekeeper-templates.rendered]
+
+  depends_on = [time_sleep.wait_for_eks_addons]
 }
 
 data "template_file" "gatekeeper-constraints" {
@@ -28,5 +30,10 @@ resource "helm_release" "gatekeeper-constraints" {
   namespace = "gatekeeper-system"
   values    = [data.template_file.gatekeeper-constraints.rendered]
 
-  depends_on = [helm_release.gatekeeper-templates]
+  depends_on = [time_sleep.wait_for_eks_addons, helm_release.gatekeeper-templates]
+}
+
+resource "time_sleep" "wait_for_eks_addons" {
+  create_duration = "120s"
+  depends_on       = [module.eks_blueprints_kubernetes_addons]
 }
