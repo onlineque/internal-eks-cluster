fe2b7749f0ffdb9adff4416512d2f279835b95be
Vladimir Siman <online@clew.cz>
2023-08-25 08:41:18 +0200

    missing output

--- outputs.tf
+++ outputs.tf
@@ -25,3 +25,7 @@ output "eks_cluster_endpoint" {
 output "eks_cluster_certificate_authority_data" {
   value = module.eks.eks_cluster_certificate_authority_data
 }
+
+output "eks_cluster_auth_token" {
+  value = data.aws_eks_cluster_auth.this.token
+}
