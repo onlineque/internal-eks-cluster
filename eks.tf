f0eccd35cbaa9182734f9f3d1050e2f3e147d974
Delplace Vivian <vivian.delplace@agc.com>
2023-11-08 13:17:45 +0100

    typo

--- eks.tf
+++ eks.tf
@@ -171,7 +171,7 @@ module "ebs_csi_driver_irsa" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   version = "~> 5.20"
 
-  role_name_prefix = "${local.name}-ebs-csi-driver-"
+  role_name_prefix = "${var.cluster_name}-ebs-csi-driver-"
 
   attach_ebs_csi_policy = true
 
