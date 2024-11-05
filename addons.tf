9e48a1b9fd1ced390429bb899027c3b1d6e44e5f
Vladimir Siman <no-reply@ubk.cz>
2024-11-05 14:12:13 +0100

    remove addons

--- addons.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_eks_addon" "amazon_cloudwatch_observability" {
-  cluster_name                = var.cluster_name
-  addon_name                  = "amazon-cloudwatch-observability"
-  resolve_conflicts_on_create = "OVERWRITE"
-}
