21bc81c5e88758f9681ee055d1d4c12055ea42ce
Vladimir Siman <no-reply@ubk.cz>
2024-11-05 13:23:47 +0100

    amazon-cloudwatch-observability addon

--- eks.tf
+++ eks.tf
@@ -222,8 +222,8 @@ module "eks_blueprints_kubernetes_addons" {
   enable_metrics_server = true
 
   # Enable AWS cloudwatch Metrics
-  enable_aws_cloudwatch_metrics = true
-
+  # enable_aws_cloudwatch_metrics = true
+  
   # Enable EFS CSI driver
   enable_aws_efs_csi_driver = true
 
