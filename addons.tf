56992aa11e9b19b7edd2d4ebe7eee2d90c6f0f5e
Vladimir Siman <no-reply@ubk.cz>
2024-11-05 13:37:28 +0100

    fix

--- addons.tf
+++ addons.tf
@@ -2,17 +2,4 @@ resource "aws_eks_addon" "amazon_cloudwatch_observability" {
   cluster_name                = var.cluster_name
   addon_name                  = "amazon-cloudwatch-observability"
   resolve_conflicts_on_create = "OVERWRITE"
-
-  configuration_values = jsonencode({
-    resources = {
-      limits = {
-        cpu    = "256m"
-        memory = "150Mi"
-      }
-      requests = {
-        cpu    = "256m"
-        memory = "150Mi"
-      }
-    }
-  })
 }
